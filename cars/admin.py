b1f869cc815a69423cf06f6798909696fa11008a
brayhon <brayhonoliveira@gmail.com>
2024-04-06 20:23:34 -0300

    Add Photo Cars

--- cars/admin.py
+++ cars/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
 #Para usar a class CAR do model precisa importar da pasta CARS do arquivo MODELS.PY da classe CAR 
-from cars.models import Car 
+from cars.models import Car, Brand
 
 # Criei um class CarAdmin importei do Django a Class admin e escolhei o que deve aparecer no painel Admin
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'plate', 'value')
     search_fields = ('model', 'brand')
 
 # Fazendo aparecer no painel registrando qual Class e Configurações do Admin
 admin.site.register(Car, CarAdmin) 
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Brand, BrandAdmin)
--- cars/models.py
+++ cars/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):                        #Classe Car tem a Herança da class Model por padrão do Django
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=100)
-    brand = models.CharField(max_length=100)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
     def __str__(self):
         return self.model #Dando nome do Model "MODELO DO CARRO" para SELF função padrão do Django (Clicar pra ver o que tanto tem.)
